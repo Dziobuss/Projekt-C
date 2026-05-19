68a01b495f63c26add66092992a3a8241010bfbd
Dziobuss <01206775@pw.edu.pl>
2026-05-19 12:47:11 +0200

    Update smacof.c

--- Algorytm c/smacof.c
+++ Algorytm c/smacof.c
@@ -1,6 +1,6 @@
 #include "graph.h"
 
-// Bezpieczna odległość euklidesowa dla SMACOF
+// Bezpieczna odległość
 double safe_dist(double x1, double y1, double x2, double y2) {
     double dx = x1 - x2;
     double dy = y1 - y2;
